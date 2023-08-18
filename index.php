1f0fa32478ff7f5e5d9e3469db4d51957588e27d
Yadav <maheshyadav0170@gmail.com>
2023-08-19 00:06:27 +0530

    INITIAL COMMIT

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>PHP Sample Project</title>
+</head>
+
+<body>
+    <h1>Hello, World!</h1>
+    <?php
+        echo "This is a simple PHP project.";
+    ?>
+</body>
+
+</html>
